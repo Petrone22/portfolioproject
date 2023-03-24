b60fd22570e3caf2769564643e3af9a09b294711
Petrone22 <the1rashid@gmai.com>
2023-03-24 23:59:45 +0200

    initialized project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import Project from "./components/Project.tsx";
+import data from "./projects.json";
+import form from "./assets/images/form.png";
+import space from "./assets/images/space.png";
+import enigma from "./assets/images/enigma.png";
+import portfolio from "./assets/images/portfolio.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Hero />
+      <div className="projects-section">
+        <Project
+          title={data[1].title}
+          description={data[1].description}
+          linkgh={data[1].linkgh}
+          linkPages={data[1].linkwebsite}
+          img={space}
+        />
+        <Project
+          title={data[2].title}
+          description={data[2].description}
+          linkgh={data[2].linkgh}
+          linkPages={data[2].linkwebsite}
+          img={form}
+        />
+        <Project
+          title={data[3].title}
+          description={data[3].description}
+          linkgh={data[3].linkgh}
+          linkPages={data[3].linkwebsite}
+          img={portfolio}
+        />
+
+        <Project
+          title={data[0].title}
+          description={data[0].description}
+          linkgh={data[0].linkgh}
+          linkPages={data[0].linkwebsite}
+          img={enigma}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="nav">
+      <span className="name ">Mustafa.</span>
+      <div className="nav-links">
+        <button className="nav-btn">Work</button>
+        <button className="nav-btn">About</button>
+        <button className="nav-btn">Resume</button>
+        <button className="nav-btn">Contact</button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
