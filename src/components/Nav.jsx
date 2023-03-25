7aeab64d44f9e7cb0cbd6fcc9bf4c80a4f926616
Petrone22 <the1rashid@gmai.com>
2023-03-25 02:24:38 +0200

    updated mobile view and finalized landing page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Nav = () => {
+  const [navOpen, setNavOpen] = useState(false);
+  const [navClass, setNavClass] = useState("burgerBar");
+  const changeClass = () => {
+    if (!navOpen) {
+      setNavClass((prevClass) => "openedBurger");
+    } else if (navOpen) {
+      setNavClass((prevClass) => "burgerBar");
+    }
+  };
   return (
     <div className="nav">
       <span className="name ">Mustafa.</span>
-      <div className="nav-links">
+      <div
+        className="nav-links"
+        style={{
+          display: navOpen ? "flex" : "none",
+        }}
+      >
         <button className="nav-btn">Work</button>
         <button className="nav-btn">About</button>
         <button className="nav-btn">Resume</button>
         <button className="nav-btn">Contact</button>
+      </div>
+      <div
+        className="hamburger"
+        onClick={() => {
+          setNavOpen((prevNav) => !prevNav);
+          changeClass();
+        }}
+      >
+        <div className={navClass} id="burgerBar"></div>
+        <div className={navClass} id="burgerBar"></div>
+        <div className={navClass} id="burgerBar"></div>
       </div>
     </div>
   );
