0d67f9fa985ded9bf7c6f74a9d7392028d32d8b6
Petrone22 <the1rashid@gmai.com>
2023-03-25 15:24:39 +0200

    fixed navigation menu bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const Nav = () => {
-  const [navOpen, setNavOpen] = useState(true);
+  const [navOpen, setNavOpen] = useState(false);
   const [navClass, setNavClass] = useState("burgerBar");
+  const windowWidth = useRef(window.innerWidth);
   const changeClass = () => {
     if (!navOpen) {
       setNavClass((prevClass) => "openedBurger");
@@ -10,21 +11,37 @@ const Nav = () => {
       setNavClass((prevClass) => "burgerBar");
     }
   };
+
+  const setNav = () => {
+    if (windowWidth.current > 780) {
+      setNavOpen((prevNav) => true);
+    } else if (windowWidth.current < 780) {
+      if (navClass === "burgerBar") {
+        setNavOpen((prevNav) => false);
+      } else if (navClass === "openedBurger") {
+        setNavOpen((preVnav) => true);
+      }
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("resize", setNav);
+  }, []);
+
   return (
     <div className="nav">
       <span className="name ">Mustafa.</span>
       <div
-        className="nav-links"
+        className="nav-links nav-btns"
         style={{
           // display: navOpen ? "flex" : "none",
           opacity: navOpen ? "1" : "0",
           pointerEvents: navOpen ? "auto" : "none",
         }}
       >
-        <button className="nav-btn">Work</button>
-        <button className="nav-btn">About</button>
-        <button className="nav-btn">Resume</button>
-        <button className="nav-btn">Contact</button>
+        <button className="nav-btn nav-button">Work</button>
+        <button className="nav-btn nav-button">About</button>
+        <button className="nav-btn nav-button">Resume</button>
+        <button className="nav-btn nav-button">Contact</button>
       </div>
       <div
         className="hamburger"
